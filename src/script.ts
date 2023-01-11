599eb95005db51a07cf188f53859461c0321181a
DavidGBbr <davidgomesbrigido@gmail.com>
2023-01-11 20:30:58 -0300

    Normalizando recebidos do api

--- src/script.ts
+++ src/script.ts
@@ -1,33 +1,14 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Recusada pela operadora de cartão"
-  | "Paga"
-  | "Aguardando pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-  ["Cliente Novo"]: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json"
   );
+  if (!data) return;
 
-  if (data) {
-    data.forEach((item) => {
-      console.log(item.Nome);
-    });
-  }
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 
 handleData();
